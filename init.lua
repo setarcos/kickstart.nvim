815aa733ed31fab742b3f5748ddc7eeb2fe8942b
Pluto Yang <yangyj.ee@gmail.com>
2024-09-10 21:52:07 +0800

    feat: restore the last cursor position

--- init.lua
+++ init.lua
@@ -1034,5 +1034,7 @@ require('lazy').setup({
 
 require('luasnip.loaders.from_vscode').load({ paths = { '~/.config/nvim/snippets/' } })
 
+vim.cmd[[au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif]]
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
