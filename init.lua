53698a6351021ac9023dbb88d2fa023d702ee922
Pluto Yang <yangyj.ee@gmail.com>
2025-03-03 09:36:23 +0800

    feat: sync with the main repo.
     * re-enable treesitter, latex's problem can be fixed by install
       treesitter binary.
     * don't save history to file, I don't like undo histories across
       sessions.

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ','
 vim.g.maplocalleader = ','
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -122,14 +122,14 @@ end)
 vim.opt.breakindent = true
 
 -- Save undo history
-vim.opt.undofile = true
+vim.opt.undofile = false
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 -- Keep signcolumn on by default
-vim.opt.signcolumn = 'no'
+vim.opt.signcolumn = 'yes'
 
 -- Decrease update time
 vim.opt.updatetime = 250
@@ -963,31 +963,31 @@ require('lazy').setup({
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
---  { -- Highlight, edit, and navigate code
---    'nvim-treesitter/nvim-treesitter',
---    build = ':TSUpdate',
---    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
---    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
---    opts = {
---      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
---      -- Autoinstall languages that are not installed
---      auto_install = true,
---      highlight = {
---        enable = true,
---        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
---        --  If you are experiencing weird indenting issues, add the language to
---        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
---        additional_vim_regex_highlighting = { 'ruby' },
---      },
---      indent = { enable = true, disable = { 'ruby' } },
---    },
---    -- There are additional nvim-treesitter modules that you can use to interact
---    -- with nvim-treesitter. You should go explore a few and see what interests you:
---    --
---    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
---    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
---    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
---  },
+  { -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+    opts = {
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'latex' },
+      -- Autoinstall languages that are not installed
+      auto_install = true,
+      highlight = {
+        enable = true,
+        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+        --  If you are experiencing weird indenting issues, add the language to
+        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+        additional_vim_regex_highlighting = { 'ruby' },
+      },
+      indent = { enable = true, disable = { 'ruby' } },
+    },
+    -- There are additional nvim-treesitter modules that you can use to interact
+    -- with nvim-treesitter. You should go explore a few and see what interests you:
+    --
+    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
