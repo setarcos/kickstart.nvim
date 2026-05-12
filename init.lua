20d90419017242b55762b6dc87b7923b0e9dab9d
Pluto Yang <yangyj.ee@gmail.com>
2026-05-12 20:37:03 +0800

    feat: Install clangd, texlab, rust_analyzer via Mason

--- init.lua
+++ init.lua
@@ -781,6 +781,9 @@ do
   local ensure_installed = vim.tbl_keys(servers or {})
   vim.list_extend(ensure_installed, {
     -- You can add other tools here that you want Mason to install
+    'clangd',
+    'texlab',
+    'rust_analyzer',
   })
 
   require('mason-tool-installer').setup { ensure_installed = ensure_installed }
