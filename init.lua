0357819234566f9b383d9ca436920a31d8dbfb4b
Pluto Yang <yangyj.ee@gmail.com>
2026-05-12 21:32:01 +0800

    feat: Restore cursor position on file reopening

--- init.lua
+++ init.lua
@@ -1007,6 +1007,17 @@ do
   -- require 'custom.plugins'
 end
 
+-- Restore cursor position when re-opening files
+vim.api.nvim_create_autocmd("BufReadPost", {
+    callback = function()
+        local mark = vim.api.nvim_buf_get_mark(0, '"')
+        local lcount = vim.api.nvim_buf_line_count(0)
+        if mark[1] > 0 and mark[1] <= lcount then
+            vim.api.nvim_win_set_cursor(0, mark)
+        end
+    end,
+})
+
 vim.pack.add{ gh 'lervag/vimtex' }
 
 -- The line beneath this is called `modeline`. See `:help modeline`
